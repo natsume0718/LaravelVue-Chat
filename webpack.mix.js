88f2c9c3c0a0113dce9a3e99198370eb2758e7db
vagrant <vagrant@homestead>
2020-03-26 08:41:14 +0000

    [add] polyfill インストール

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,10 @@
 const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+require('laravel-mix-polyfill');
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .polyfill({
+        enabled: true,
+        useBuiltIns: "usage",
+        targets: { "firefox": "50", "ie": 11 }
+    });;
