df3193c2fc52d11959b16946584d0ed743db9a16
vagrant <vagrant@homestead>
2020-03-26 09:21:28 +0000

    [add] purgecssインストール

--- README.md
+++ README.md
@@ -42,3 +42,13 @@ Laravel 6.x LTS のデバッグやコード解析を導入した Sandbox
 -   debugbar : 画面にデバッグ用の表示を行う
 
 -   laravel-test-factory-helper : モデルから簡単にファクトリーを作成する
+
+## Frontend Develop
+
+-   `frontend`ブランチをマージします。
+
+-   `npm install`
+
+-   `Purgecss`で未使用 css を削除します
+
+-   `Polyfill`で対象の browser のサポートを行います
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,10 @@
 const mix = require('laravel-mix');
 require('laravel-mix-polyfill');
+require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .purgeCss()
     .polyfill({
         enabled: true,
         useBuiltIns: "usage",
