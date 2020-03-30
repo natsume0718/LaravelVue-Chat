3c61a5a14747848c10cd0c3dcf2f6450b5c11c73
natsume0718 <moto718program@gmail.com>
2020-03-30 23:21:35 +0900

    [add] webpackの設定追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,3 +19,19 @@ mix.js("resources/js/app.js", "public/js")
         useBuiltIns: "usage",
         targets: { firefox: "50", ie: 11 }
     });
+// ESLintに関する設定（この部分を丸ごと追記するイメージです）
+if (!mix.inProduction()) {
+    // 本番環境ではESLintは使用しない
+    mix.webpackConfig({
+        module: {
+            rules: [
+                {
+                    enforce: "pre",
+                    exclude: /node_modules/,
+                    loader: "eslint-loader",
+                    test: /\.(js|vue)?$/
+                }
+            ]
+        }
+    });
+}
